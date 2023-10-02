306903953fcd28dcc275a798feacc20edd98e304
cristianromano <63170214+cristianromano@users.noreply.github.com>
2023-10-02 17:27:59 -0300

    se sube codigo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomePageModule } from './home/home.module';
+import { HomePage } from './home/home.page';
+import { LoginPageModule } from './pages/login/login.module';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
     path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
+    component: HomePage,
   },
   {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    loadChildren: () =>
+      import('./pages/login/login.module').then((m) => m.LoginPageModule),
   },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'practica-profesional-5cee0',
+    appId: '1:19344312290:web:cde04a71f2d452208adbbb',
+    storageBucket: 'practica-profesional-5cee0.appspot.com',
+    apiKey: 'AIzaSyC8Ac7F_PrFszswcKXB8SFvh0G4poI9-VM',
+    authDomain: 'practica-profesional-5cee0.firebaseapp.com',
+    messagingSenderId: '19344312290',
+  },
   production: false
 };
 
